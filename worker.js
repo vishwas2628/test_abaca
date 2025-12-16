ec125e43ac55862b04e103ccb3671c62cac17560
vishwaschourasiya <vishwas.chourasiya@reak.in>
2025-12-16 13:34:06 +0530

    rename worker to .js and use commonjs

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ const app = require('./index.js');
 
 const handler = serverless(app);
 
-export default {
+module.exports = {
     fetch(request, env, ctx) {
         return handler(request, env, ctx);
     }
